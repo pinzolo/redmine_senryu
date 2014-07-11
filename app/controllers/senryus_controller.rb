05fcf6b29badcb0f6fac6b7a53dfe1d7fca0dc28
pinzolo <pinzolo@gmail.com>
2014-07-12 02:10:47 +0900

    Enable to create senryu

--- app/controllers/senryus_controller.rb
+++ app/controllers/senryus_controller.rb
@@ -1,13 +1,27 @@
 class SenryusController < ApplicationController
   unloadable
 
+  before_filter :find_project, :authorize
 
   def index
+    @senryus = Senryu.all
   end
 
   def new
+    @senryu = Senryu.new
   end
 
   def create
+    @senryu = Senryu.new(params[:senryu])
+    @senryu.save
+    redirect_to project_senryus_path(@project)
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:project_id])
+  rescue ActiveRecord::RecordNotFound
+    render_404
   end
 end
--- init.rb
+++ init.rb
@@ -5,4 +5,10 @@ Redmine::Plugin.register :redmine_senryu do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+  project_module :senryu do
+    permission :view_senryu, senryus: [:index]
+    permission :manage_senryu, senryus: [:new, :create]
+  end
+  menu :project_menu, :senryu, { controller: :senryus, action: :index }, param: :project_id, caption: :project_module_senryu
 end
